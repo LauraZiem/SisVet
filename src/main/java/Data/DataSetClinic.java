80bf853576bcf63b22f9700860dcddef0eaa092c
LauraZiem <laura.ziem12@gmail.com>
2022-05-03 19:31:49 -0300

    Controller/Data

---
+++ src/main/java/Data/DataSetClinic.java
@@ -0,0 +1,9 @@
+package Data;
+
+import Model.Clinic;
+import java.util.ArrayList;
+
+public abstract class DataSetClinic {
+    public static ArrayList<Clinic> data = new ArrayList<Clinic>();
+    public static Clinic selectedClinic;
+}
